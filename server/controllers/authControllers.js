1e520a844eedf227e1844fd010df894e6c7fc989
Jonathan <JohnAnBer@gmail.com>
2026-05-21 07:01:38 -0400

    CORS

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -22,7 +22,7 @@ const COOKIE_NAME = "token";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
 };
 
